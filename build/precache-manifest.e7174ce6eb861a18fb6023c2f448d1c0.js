ffef80f3e35fcc66aa92653361cb22647f570274
Alexey Brambalov <brambalov@gmail.com>
2019-07-17 22:27:49 +0100

    first commit

---
+++ build/precache-manifest.e7174ce6eb861a18fb6023c2f448d1c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba7e59db85dc721812b343a9f4f0a1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6690b880d771c07a706",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "96f439316f88c2bb451b",
+    "url": "/static/js/2.4c7503a7.chunk.js"
+  },
+  {
+    "revision": "f6690b880d771c07a706",
+    "url": "/static/js/main.558e15f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
